65820b343e92f1d2b46c140d17057b29f404a395
ivanlori <ivan.lori1@gmail.com>
2019-07-22 07:42:10 +0200

    vat calculator

--- src/components/vat/VAT.container.tsx
+++ src/components/vat/VAT.container.tsx
@@ -4,27 +4,39 @@ import { connect } from 'react-redux'
 import { InputView } from '../input/Input.view'
 import { Label } from '../../styles/global.style'
 import { Wrapper, Percentage } from './VAT.style'
-import { changeVat } from '../../store/actions'
-
-const mapDispatchToProps = (dispatch: any) => {
-  return {
-    setVATtoProps: (value: any) => {
-      dispatch(changeVat(value))
-    }
-  }
-}
+import { changeVat, totalAmount } from '../../store/actions'
 
 interface Props {
-  setVATtoProps: (event: React.ChangeEvent<HTMLInputElement>) => void
+  setVATtoProps: (value: string) => void
+  setAmountWithTaxes: (value: any) => void,
+  amountWithoutTaxes: string
 }
 
 class VatContainer extends Component<Props> {
 
-  vatHandler = (e: any) => {
+
+  vatHandler = (e: any): void => {
     if (e.target.value !== '') {
-      this.props.setVATtoProps(e.target.value)
+      let vatValue = e.target.value
+
+      this.setState({
+        vat: e.target.value
+      })
+      this.amountWithTaxesCalc(vatValue)
+      this.props.setVATtoProps(vatValue)
     }
   }
+
+  amountWithTaxesCalc = (vat: number): void => {
+    let amountWithoutTaxesParsed = parseFloat(this.props.amountWithoutTaxes)
+    let vatCalculated = this.calculateVat(amountWithoutTaxesParsed, vat)
+    let total = amountWithoutTaxesParsed + vatCalculated
+    this.props.setAmountWithTaxes(total)
+  }
+
+  calculateVat (amount: number, percentage: number): number {
+    return (amount * percentage) / 100
+  } 
 
   render() {
     return (
@@ -41,4 +53,21 @@ class VatContainer extends Component<Props> {
   }
 }
 
-export default connect(null, mapDispatchToProps)(VatContainer)
+const mapDispatchToProps = (dispatch: any) => {
+  return {
+    setVATtoProps: (value: any) => {
+      dispatch(changeVat(value))
+    },
+    setAmountWithTaxes: (value: any) => {
+      dispatch(totalAmount(value))
+    }
+  }
+}
+
+const mapStateToProps = (state: any) => {
+	return {
+    amountWithoutTaxes: state.amountWithoutTaxes
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(VatContainer)
